b47c1c8ac4e42ab096b86bf7b220d0ec880cdcb4
Roshan Piyush <piyushroshan@gmail.com>
2013-03-14 21:04:28 +0530

    added tabs

--- src/software/component/cataloging/Main.java
+++ src/software/component/cataloging/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-                NewJFrame frame= new NewJFrame();
+                GUI frame= new GUI();
                 frame.setVisible(true);
     }
 }
