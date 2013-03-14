ea056445ebdfbba5add4c1dcefd275ada2a6c4a2
Roshan Piyush <piyushroshan@gmail.com>
2013-03-14 19:28:01 +0530

    Starting GUI

---
+++ src/software/component/cataloging/Main.java
@@ -0,0 +1,22 @@
+package software.component.cataloging;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author R0$H4N
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                NewJFrame frame= new NewJFrame();
+                frame.setVisible(true);
+    }
+}
