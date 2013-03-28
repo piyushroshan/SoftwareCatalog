ce5f033c648fd1aab58a91ca048ff18bb3f915bc
Roshan Piyush <piyushroshan@gmail.com>
2013-03-28 21:34:34 +0530

    readme updated

--- src/software/component/cataloging/AddComponent.java
+++ src/software/component/cataloging/AddComponent.java
@@ -19,7 +19,7 @@ public class AddComponent extends javax.swing.JPanel implements ActionListener {
      */
     public AddComponent(String[] code, String[] design) {
         initComponents();
-        jComboBox1.addActionListener(this);
+        choiceCategoryField.addActionListener(this);
         this.code = code;
         System.out.print(code);
         this.design = design;
@@ -34,56 +34,56 @@ public class AddComponent extends javax.swing.JPanel implements ActionListener {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jLabel2 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox();
-        jComboBox2 = new javax.swing.JComboBox();
-        jLabel3 = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
+        nameLabel = new javax.swing.JLabel();
+        nameField = new javax.swing.JTextField();
+        typeLabel = new javax.swing.JLabel();
+        choiceCategoryField = new javax.swing.JComboBox();
+        choiceSubCatagoryField = new javax.swing.JComboBox();
+        descriptionLabel = new javax.swing.JLabel();
+        descriptionText = new javax.swing.JScrollPane();
         jTextPane1 = new javax.swing.JTextPane();
-        jLabel4 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jLabel5 = new javax.swing.JLabel();
+        contentLabel = new javax.swing.JLabel();
+        saveButton = new javax.swing.JButton();
+        cancelButton = new javax.swing.JButton();
+        chooseFileButton = new javax.swing.JButton();
+        fileLabel = new javax.swing.JLabel();
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
-        jLabel1.setText("Name");
+        nameLabel.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
+        nameLabel.setText("Name");
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
-        jLabel2.setText("Type");
+        typeLabel.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
+        typeLabel.setText("Type");
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "----", "Code", "Design" }));
+        choiceCategoryField.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "----", "Code", "Design" }));
 
-        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "----" }));
+        choiceSubCatagoryField.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "----" }));
 
-        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
-        jLabel3.setText("Description");
+        descriptionLabel.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
+        descriptionLabel.setText("Description");
 
         jTextPane1.setVerifyInputWhenFocusTarget(false);
-        jScrollPane1.setViewportView(jTextPane1);
+        descriptionText.setViewportView(jTextPane1);
 
-        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
-        jLabel4.setText("Content");
+        contentLabel.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
+        contentLabel.setText("Content");
 
-        jButton1.setText("Save");
+        saveButton.setText("Save");
 
-        jButton2.setText("Cancel");
-        jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
+        cancelButton.setText("Cancel");
+        cancelButton.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jButton2MouseClicked(evt);
+                cancelButtonMouseClicked(evt);
             }
         });
 
-        jButton3.setText("Choose File");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        chooseFileButton.setText("Choose File");
+        chooseFileButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                chooseFileButtonActionPerformed(evt);
             }
         });
 
-        jLabel5.setText("Filename");
+        fileLabel.setText("Filename");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -92,24 +92,24 @@ public class AddComponent extends javax.swing.JPanel implements ActionListener {
             .addGroup(layout.createSequentialGroup()
                 .addGap(50, 50, 50)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(nameLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(descriptionLabel)
+                    .addComponent(contentLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(saveButton)
+                    .addComponent(typeLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(28, 28, 28)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(choiceCategoryField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(41, 41, 41)
-                        .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 639, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(choiceSubCatagoryField, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(nameField, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(descriptionText, javax.swing.GroupLayout.PREFERRED_SIZE, 639, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 198, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(fileLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 198, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(56, 56, 56)
-                        .addComponent(jButton3))
-                    .addComponent(jButton2))
+                        .addComponent(chooseFileButton))
+                    .addComponent(cancelButton))
                 .addContainerGap(243, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -117,77 +117,77 @@ public class AddComponent extends javax.swing.JPanel implements ActionListener {
             .addGroup(layout.createSequentialGroup()
                 .addGap(42, 42, 42)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(nameLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(nameField, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jLabel2))
-                    .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(choiceCategoryField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(typeLabel))
+                    .addComponent(choiceSubCatagoryField, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(29, 29, 29)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 349, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(descriptionText, javax.swing.GroupLayout.PREFERRED_SIZE, 349, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(61, 61, 61)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addComponent(jLabel3))
+                            .addComponent(fileLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(chooseFileButton, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(contentLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(descriptionLabel))
                 .addGap(70, 70, 70)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
+                    .addComponent(saveButton)
+                    .addComponent(cancelButton))
                 .addContainerGap(121, Short.MAX_VALUE))
         );
 
-        jComboBox2.setVisible(false);
+        choiceSubCatagoryField.setVisible(false);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
+    private void cancelButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_cancelButtonMouseClicked
         // TODO add your handling code here:
         
-    }//GEN-LAST:event_jButton2MouseClicked
+    }//GEN-LAST:event_cancelButtonMouseClicked
 
-private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+private void chooseFileButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_chooseFileButtonActionPerformed
 // TODO add your handling code here:
     FileDialogProgram fileD = new FileDialogProgram();
     File file = fileD.openDialog();
-}//GEN-LAST:event_jButton3ActionPerformed
+}//GEN-LAST:event_chooseFileButtonActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JComboBox jComboBox1;
-    private javax.swing.JComboBox jComboBox2;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JButton cancelButton;
+    private javax.swing.JComboBox choiceCategoryField;
+    private javax.swing.JComboBox choiceSubCatagoryField;
+    private javax.swing.JButton chooseFileButton;
+    private javax.swing.JLabel contentLabel;
+    private javax.swing.JLabel descriptionLabel;
+    private javax.swing.JScrollPane descriptionText;
+    private javax.swing.JLabel fileLabel;
     private javax.swing.JTextPane jTextPane1;
+    private javax.swing.JTextField nameField;
+    private javax.swing.JLabel nameLabel;
+    private javax.swing.JButton saveButton;
+    private javax.swing.JLabel typeLabel;
     // End of variables declaration//GEN-END:variables
 
     @Override
  public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == jComboBox1){
-        String text = jComboBox1.getModel().getSelectedItem().toString();
+        if(e.getSource() == choiceCategoryField){
+        String text = choiceCategoryField.getModel().getSelectedItem().toString();
             switch (text) {
                 case "Code":
-                    jComboBox2.setModel(new javax.swing.DefaultComboBoxModel(code));
-                    jComboBox2.setVisible(true);
+                    choiceSubCatagoryField.setModel(new javax.swing.DefaultComboBoxModel(code));
+                    choiceSubCatagoryField.setVisible(true);
                     break;
                 case "Design":
-                    jComboBox2.setModel(new javax.swing.DefaultComboBoxModel(design));
-                    jComboBox2.setVisible(true);
+                    choiceSubCatagoryField.setModel(new javax.swing.DefaultComboBoxModel(design));
+                    choiceSubCatagoryField.setVisible(true);
                     break;
                 default:
-                    jComboBox2.setModel(new javax.swing.DefaultComboBoxModel());
-                    jComboBox2.setVisible(false);
+                    choiceSubCatagoryField.setModel(new javax.swing.DefaultComboBoxModel());
+                    choiceSubCatagoryField.setVisible(false);
             }
         }
        // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
